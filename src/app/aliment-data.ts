1e27dbf366fa271aef1398ee3be6edc958a6e759
unknown <stephane.rodolausse.pf1@gmail.com>
2018-06-15 15:37:14 +0200

    creation d'un fichier collection d'aliments

---
+++ src/app/aliment-data.ts
@@ -0,0 +1,75 @@
+const ALIMENT_DATA = {
+    aliments: Aliment[
+        {
+            id: 1,
+            name: 'Abricot',
+            category: 'fruits frais',
+            energy: '42,6',
+            ig: '30',
+            protein: '1,86',
+            carb: '39,8',
+            sugar: '36,6',
+            lipid: '0,51',
+            fibre: '5,7',
+            salt: '0,098',
+            CG: ''
+        },
+        {
+            id: 2,
+            name: 'Ananas',
+            category: 'fruits frais',
+            energy: '53,2',
+            ig: '65',
+            protein: '0,52',
+            carb: '11',
+            sugar: '10,4',
+            lipid: '0,24',
+            fibre: '1,33',
+            salt: '0,0024',
+            CG: ''
+        },
+        {
+            id: 3,
+            name: 'Artichaud',
+            category: 'légumes cuits',
+            energy: '33,2',
+            ig: '20',
+            protein: '2,53',
+            carb: '0,99',
+            sugar: '0,99',
+            lipid: '0,28',
+            fibre: '8,3',
+            salt: '0,14',
+            CG: ''
+        },
+        {
+            id: 4,
+            name: 'Banane',
+            category: 'fruits frais',
+            energy: '90',
+            ig: '65',
+            protein: '0,98',
+            carb: '19,6',
+            sugar: '14,8',
+            lipid: '0,25',
+            fibre: '1,9',
+            salt: '0,0038',
+            CG: ''
+        },
+        {
+            id: 5,
+            name: 'Bar commun ou loup',
+            category: 'poissons cuits',
+            energy: '147',
+            ig: '',
+            protein: '22,2',
+            carb: '',
+            sugar: '0',
+            lipid: '6,51',
+            fibre: '0',
+            salt: '0,0038',
+            CG: ''
+        }
+    ]
+};
+export default ALIMENT_DATA;
