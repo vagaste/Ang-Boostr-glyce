1e6c63ae5405dd849c65d8533c3c3ead068d7520
Valerie-LABROUSSE <36170148+Valerie-LABROUSSE@users.noreply.github.com>
2018-07-04 17:33:10 +0200

    calcul charge glycémique

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -14,28 +14,42 @@ import { Sort, MatPaginator, MatTableDataSource} from '@angular/material';
 export class BodyComponent implements OnInit {
 quantityAliment: number;
 alimentSelected: Aliment;
-resultCg;
+resultCg: number;
 
 displayedColumns = ['name', 'category', 'energy', 'ig', 'cg'];
 listAliments = this.alimentService.searchedAliments;
 sortedAliments = new MatTableDataSource < Aliment >(this.listAliments);
 @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  // (val) ici on injecte le service AlimentService : nom + type. L'injection permet de
+  // ici on injecte le service AlimentService : nom + type. L'injection permet de
   // déclarer ici l'objet "aliments"
   constructor(public alimentService: AlimentService) {
 
   }
 
   ngOnInit () {
-    // (val) le composant est prêt à être utilisé : mettre son code ici
+    // le composant est prêt à être utilisé : mettre son code ici
     // l'objet aliment est alimenté avec l'objet aliment déclaré dans le service alimentService
     // this.sortedAliments.paginator = this.paginator;
-  }
 
-  /* ngAfterViewInit() {
-  this.sortedAliments.paginator = this.paginator;
-  } */
+    this.alimentSelected = {
+      id: 0,
+      name: '',
+      category: '',
+      energy: 0,
+      ig: 0,
+      protein: 0,
+      carb: 0,
+      sugar: 0,
+      lipid: 0,
+      fibre: 0,
+      salt: 0,
+      cg: 0
+  };
+
+  this.resultCg = 0;
+  this.quantityAliment = 0;
+  }
 
   sortData(sort: Sort) {
     const data = this.alimentService.searchedAliments.slice();
@@ -55,6 +69,19 @@ sortedAliments = new MatTableDataSource < Aliment >(this.listAliments);
         }
       });
   }
+  selectAliment (aliment, e) {
+      e.preventDefault();
+      this.alimentSelected = aliment ;
+
+  }
+
+  calculCg (alimentSelected, quantity) {
+    if (alimentSelected.ig === '') {
+    this.resultCg = 0;
+    }
+
+    this.resultCg = ((alimentSelected.ig * ((quantity * alimentSelected.carb) / 100)) / 100);
+  }
 }
 
 function comparenum(a, b, isAsc) {
@@ -65,21 +92,6 @@ function compare(a, b, isAsc) {
 }
 // quantite de glucide(qg), pour la quantite de l aliment saisie (quantite de l aliment*quantite de glucide pour 100g)/100
 // formule (ig*qg)/100
-/* public calculCg (alimentSelected, quantityAliment) {
-    if (alimentSelected.ig === '') {
-      return 0;
-    }
-    return this.resultCg = ((alimentSelected.ig * ((this.quantityAliment * alimentSelected.carb) / 100)) / 100);
-  } */
-/* (val) méthode pour afficher le détail : il faut aussi ajouter
-private router: Router
-dans le constructeur mais ça plante à l'exécution
 
-  showAlimentDetail(aliment, e) {
-    e.preventDefault();
-     this.alimentService.selectedAliment = aliment ;
-     this.router.navigateByUrl('/detail');
 
-  }
-  */
 
