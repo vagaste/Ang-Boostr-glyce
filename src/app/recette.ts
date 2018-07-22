19aaabe1fbbf5535e7487db8ba27365377e51388
Valerie-LABROUSSE <36170148+Valerie-LABROUSSE@users.noreply.github.com>
2018-07-22 23:44:00 +0200

    création recette avec ano

--- src/app/recette.ts
+++ src/app/recette.ts
@@ -1,16 +1,16 @@
 import { Portion } from 'src/app/portion';
 
 export interface Recette {
-    id: number;
+    id?: number;
     name: string;
-    comment: string;
-    energy: number;
+    comment?: string;
+    energy?: number;
     cg: number;
-    protein: number;
-    carb: number;
-    lipid: number;
-    fibre: number;
-    sugar: number;
-    salt: number;
+    protein?: number;
+    carb?: number;
+    lipid?: number;
+    fibre?: number;
+    sugar?: number;
+    salt?: number;
     portions: Portion[];
 }
--- src/app/recette/recette.component.ts
+++ src/app/recette/recette.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith, tap} from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith, tap } from 'rxjs/operators';
 import { Aliment } from '../aliment';
 import { AlimentService } from '../aliment.service';
 import { RecetteService } from '../recette.service';
 import { PortionService } from '../portion.service';
+import { Recette } from '../recette';
 
 @Component({
   selector: 'app-recette',
@@ -21,7 +22,8 @@ export class RecetteComponent implements OnInit {
   quantityPortion: number;
   resultCg: number;
   cgRecette: number;
-  recette: RecetteService;
+  recette: Recette;
+  // recette: RecetteService
   portion: PortionService;
   tableauPortion = [];
   selectedAliment: Aliment;
@@ -30,42 +32,47 @@ export class RecetteComponent implements OnInit {
     public recetteService: RecetteService, public portionService: PortionService) { }
 
   ngOnInit() {
-// ici on alimente option avec l'aliment service pour l'autocomplete
-// mettre un subscribe quand on communiquera avec le back + mettre l'url du
-// back dans le service
+    // ici on alimente option avec l'aliment service pour l'autocomplete
+    // mettre un subscribe quand on communiquera avec le back + mettre l'url du
+    // back dans le service
 
     this.alimentService.getAll().subscribe((listAliments: Aliment[]) => {
-    this.options = listAliments;
-    this.filteredOptions = this.myControl.valueChanges
-    .pipe(
-      startWith<string | Aliment>(''),
-      tap(value => {
-        if (typeof value !== 'string') {
-        this.selectedAliment = value;
-        }
-        }),
-      map(value => typeof value === 'string' ? value : value.name),
-      map(name => name ? this._filter(name) : this.options.slice())
-    );
+      this.options = listAliments;
+      this.filteredOptions = this.myControl.valueChanges
+        .pipe(
+          startWith<string | Aliment>(''),
+          tap(value => {
+            if (typeof value !== 'string') {
+              this.selectedAliment = value;
+            }
+          }),
+          map(value => typeof value === 'string' ? value : value.name),
+          map(name => name ? this._filter(name) : this.options.slice())
+        );
 
     });
-    this.recette = this.recetteService;
+    // this.recette = this.recetteService;
     this.cptr = 0;
     this.quantityPortion = 0;
     this.resultCg = 0;
     this.cgRecette = 0;
 
-      this.portion = {
-        id: null,
-        quantity: 0,
-        fk_idaliment: null,
-        fk_idrecette: null
-      };
-      }
+    this.recette = {
+      name: '',
+      cg: 0
+    };
 
-  stockPortion () {
+    this.portion = {
+      id: null,
+      quantity: 0,
+      fk_idaliment: null,
+      fk_idrecette: null
+    };
+  }
 
-    this.resultCg =  ((this.selectedAliment.ig *
+  stockPortion() {
+
+    this.resultCg = ((this.selectedAliment.ig *
       ((this.quantityPortion * this.selectedAliment.carb) / 100)) / 100);
     this.cgRecette = this.cgRecette + this.resultCg;
     this.cptr = this.cptr + 1;
@@ -78,6 +85,23 @@ export class RecetteComponent implements OnInit {
     };
 
     this.tableauPortion.push(tabPortion);
+
+  }
+
+  create() {
+
+    this.recette.cg = this.cgRecette;
+    console.log('recette cg = ' + this.recette.cg);
+    console.log('recette NAME = ' + this.recette.name);
+
+    for (const tabPortion of this.tableauPortion) {
+      this.recette.portions = tabPortion;
+      console.log(tabPortion);
+    }
+    this.recetteService.createRecette(this.recette)
+      .subscribe((recette: Recette) => {
+        this.recette = recette;
+      });
 
   }
 
