c62e7717ec3dc6a7d321d17e6de477a49e95be0b
unknown <stephane.rodolausse.pf1@gmail.com>
2018-07-26 22:12:13 +0200

    Ajout methode delete sur list componant recette

--- src/app/list-recettes/list-recettes.component.ts
+++ src/app/list-recettes/list-recettes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecetteService } from 'src/app/recette.service';
 import { Recette } from 'src/app/recette';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-list-recettes',
@@ -11,9 +12,29 @@ export class ListRecettesComponent implements OnInit {
 
 listRecette: Recette[] = [];
 selectedRecette: Recette;
-  constructor(public recetteService: RecetteService) { }
+recetteId: number;
+  constructor(public recetteService: RecetteService, public router: Router) {
+    
+     // override the route reuse strategy
+     this.router.routeReuseStrategy.shouldReuseRoute = function(){
+      return false;
+   }
+
+   this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+         // trick the Router into believing it's last link wasn't previously loaded
+         this.router.navigated = false;
+         // if you need to scroll back to top, here is the right place
+         window.scrollTo(0, 0);
+      }
+  });
+   }
 
   ngOnInit() {
+    this.getListRecette();
+    }
+
+  getListRecette() {
     this.recetteService.getAll().subscribe((listRecette: Recette[]) => {
       this.listRecette =  listRecette;
       if (this.listRecette && this.listRecette.length > 0) {
@@ -25,5 +46,15 @@ selectedRecette: Recette;
   selectRecette(selectedRecette: Recette) {
     this.selectedRecette = selectedRecette ;
   }
-
+  deleteRecette(index: number) {
+    console.log(index);
+    this.recetteId = this.listRecette[index].id;
+    console.log(this.recetteId);
+    this.recetteService.removeRecetteById(this.recetteId).subscribe(() => {
+      this.router.navigateByUrl('/recette/liste');
+    }, (err) => {
+      console.log('erreur !', err);
+    }
+    );
+  }
 }
